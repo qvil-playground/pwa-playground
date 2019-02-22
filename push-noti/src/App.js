46ec3abe19349534b674aec02d64f09ffc9faf61
Taesu Hyeon <qvil1127@gmail.com>
2019-02-22 22:01:11 +0900

    Change class component to hooks component
    - Test useEffect(componentDidMount) timing

--- push-noti/src/App.js
+++ push-noti/src/App.js
@@ -1,42 +1,50 @@
-import React, { Component } from "react";
-import convertDataURIToBinary from "./lib/base64";
+import React, { useEffect } from "react";
+// import convertDataURIToBinary from "./lib/base64";
 
-class App extends Component {
-  componentDidMount() {
-    console.log(
-      "TCL: App -> componentDidMount -> process.env.REACT_APP_APPLICATION_SERVER_KEY",
-      process.env.REACT_APP_APPLICATION_SERVER_KEY
-    );
-    if ("serviceWorker" in navigator && "PushManager" in window) {
-      navigator.serviceWorker
-        .register("/service-worker.js")
-        .then(swReg => {
-          console.log("SW Registered: ", swReg);
-          Notification.requestPermission().then(permission => {
-            if (permission === "granted") {
-              console.log("Permission: ", permission);
-              swReg.pushManager
-                .subscribe({
-                  userVisibleOnly: true,
-                  applicationServerKey: convertDataURIToBinary(
-                    process.env.REACT_APP_APPLICATION_SERVER_KEY
-                  )
-                })
-                .then(pushSubscriptionObject =>
-                  console.log(pushSubscriptionObject)
-                )
-                .catch(error =>
-                  console.error("TCL: App -> componentDidMount -> error", error)
-                );
-            }
-          });
-        })
-        .catch(error => console.error("Can't register SW: ", error));
-    }
-  }
-  render() {
-    return <div>App</div>;
-  }
+export default function App() {
+  useEffect(() => {
+    console.log("componentDidMount");
+  });
+  console.log("render");
+  return <div>Hello</div>;
 }
 
-export default App;
+// class App extends Component {
+//   componentDidMount() {
+//     console.log(
+//       "TCL: App -> componentDidMount -> process.env.REACT_APP_APPLICATION_SERVER_KEY",
+//       process.env.REACT_APP_APPLICATION_SERVER_KEY
+//     );
+//     if ("serviceWorker" in navigator && "PushManager" in window) {
+//       navigator.serviceWorker
+//         .register("/service-worker.js")
+//         .then(swReg => {
+//           console.log("SW Registered: ", swReg);
+//           Notification.requestPermission().then(permission => {
+//             if (permission === "granted") {
+//               console.log("Permission: ", permission);
+//               swReg.pushManager
+//                 .subscribe({
+//                   userVisibleOnly: true,
+//                   applicationServerKey: convertDataURIToBinary(
+//                     process.env.REACT_APP_APPLICATION_SERVER_KEY
+//                   )
+//                 })
+//                 .then(pushSubscriptionObject =>
+//                   console.log(pushSubscriptionObject)
+//                 )
+//                 .catch(error =>
+//                   console.error("TCL: App -> componentDidMount -> error", error)
+//                 );
+//             }
+//           });
+//         })
+//         .catch(error => console.error("Can't register SW: ", error));
+//     }
+//   }
+//   render() {
+//     return <div>App</div>;
+//   }
+// }
+
+// export default App;
